f46fcf4dc1ae9761980fa08f4856ae480a04684f
Bryan Mayland <bmayland@capnbry.net>
2013-02-27 09:45:40 -0500

    [vis] Add visualization project

---
+++ arduino/JeeAWatt/visualize/JeeAWattVis/frmMain.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace JeeAWattVis
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serial = new System.IO.Ports.SerialPort(this.components);
+            this.chtVis = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.lblAmpBase = new System.Windows.Forms.Label();
+            this.lblVoltBase = new System.Windows.Forms.Label();
+            this.btnBase = new System.Windows.Forms.Button();
+            this.lblAmpTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnBaseSingle = new System.Windows.Forms.Button();
+            this.cbxMode = new System.Windows.Forms.ComboBox();
+            this.lblWatts = new System.Windows.Forms.Label();
+            this.lblVolts = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPowerFactor = new System.Windows.Forms.Label();
+            this.txtVoltFactor = new System.Windows.Forms.TextBox();
+            this.chkShowLabels = new System.Windows.Forms.CheckBox();
+            this.txtAmpFactor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chtVis)).BeginInit();
+            this.pnlLeft.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serial
+            // 
+            this.serial.BaudRate = 115200;
+            this.serial.PortName = "COM3";
+            // 
+            // chtVis
+            // 
+            this.chtVis.BorderlineColor = System.Drawing.Color.MidnightBlue;
+            this.chtVis.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.chtVis.BorderlineWidth = 2;
+            this.chtVis.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Emboss;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.chtVis.ChartAreas.Add(chartArea1);
+            this.chtVis.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chtVis.Location = new System.Drawing.Point(113, 0);
+            this.chtVis.Margin = new System.Windows.Forms.Padding(0);
+            this.chtVis.Name = "chtVis";
+            this.chtVis.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsValueShownAsLabel = true;
+            series1.MarkerSize = 1;
+            series1.Name = "srsVolt";
+            series1.ShadowOffset = 1;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsValueShownAsLabel = true;
+            series2.Name = "srsAmp";
+            series2.ShadowOffset = 1;
+            this.chtVis.Series.Add(series1);
+            this.chtVis.Series.Add(series2);
+            this.chtVis.Size = new System.Drawing.Size(542, 434);
+            this.chtVis.TabIndex = 1;
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.Controls.Add(this.lblPowerFactor);
+            this.pnlLeft.Controls.Add(this.txtVoltFactor);
+            this.pnlLeft.Controls.Add(this.label2);
+            this.pnlLeft.Controls.Add(this.lblVolts);
+            this.pnlLeft.Controls.Add(this.lblWatts);
+            this.pnlLeft.Controls.Add(this.cbxMode);
+            this.pnlLeft.Controls.Add(this.btnBaseSingle);
+            this.pnlLeft.Controls.Add(this.lblCount);
+            this.pnlLeft.Controls.Add(this.chkShowLabels);
+            this.pnlLeft.Controls.Add(this.lblCurrent);
+            this.pnlLeft.Controls.Add(this.label1);
+            this.pnlLeft.Controls.Add(this.txtAmpFactor);
+            this.pnlLeft.Controls.Add(this.lblAmpTotal);
+            this.pnlLeft.Controls.Add(this.btnBase);
+            this.pnlLeft.Controls.Add(this.lblVoltBase);
+            this.pnlLeft.Controls.Add(this.lblAmpBase);
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(113, 434);
+            this.pnlLeft.TabIndex = 0;
+            // 
+            // lblAmpBase
+            // 
+            this.lblAmpBase.AutoSize = true;
+            this.lblAmpBase.Location = new System.Drawing.Point(12, 9);
+            this.lblAmpBase.Name = "lblAmpBase";
+            this.lblAmpBase.Size = new System.Drawing.Size(62, 13);
+            this.lblAmpBase.TabIndex = 0;
+            this.lblAmpBase.Text = "lblAmpBase";
+            // 
+            // lblVoltBase
+            // 
+            this.lblVoltBase.AutoSize = true;
+            this.lblVoltBase.Location = new System.Drawing.Point(12, 22);
+            this.lblVoltBase.Name = "lblVoltBase";
+            this.lblVoltBase.Size = new System.Drawing.Size(59, 13);
+            this.lblVoltBase.TabIndex = 1;
+            this.lblVoltBase.Text = "lblVoltBase";
+            // 
+            // btnBase
+            // 
+            this.btnBase.Location = new System.Drawing.Point(3, 379);
+            this.btnBase.Name = "btnBase";
+            this.btnBase.Size = new System.Drawing.Size(107, 23);
+            this.btnBase.TabIndex = 2;
+            this.btnBase.Text = "Full Baseline";
+            this.btnBase.UseVisualStyleBackColor = true;
+            this.btnBase.Click += new System.EventHandler(this.btnBase_Click);
+            // 
+            // lblAmpTotal
+            // 
+            this.lblAmpTotal.AutoSize = true;
+            this.lblAmpTotal.Location = new System.Drawing.Point(12, 47);
+            this.lblAmpTotal.Name = "lblAmpTotal";
+            this.lblAmpTotal.Size = new System.Drawing.Size(62, 13);
+            this.lblAmpTotal.TabIndex = 3;
+            this.lblAmpTotal.Text = "lblAmpTotal";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amp Factor";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrent.Location = new System.Drawing.Point(12, 229);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(52, 17);
+            this.lblCurrent.TabIndex = 5;
+            this.lblCurrent.Text = "0 amps";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(12, 34);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(45, 13);
+            this.lblCount.TabIndex = 7;
+            this.lblCount.Text = "lblCount";
+            // 
+            // btnBaseSingle
+            // 
+            this.btnBaseSingle.Location = new System.Drawing.Point(3, 408);
+            this.btnBaseSingle.Name = "btnBaseSingle";
+            this.btnBaseSingle.Size = new System.Drawing.Size(107, 23);
+            this.btnBaseSingle.TabIndex = 8;
+            this.btnBaseSingle.Text = "1-Shot Baseline";
+            this.btnBaseSingle.UseVisualStyleBackColor = true;
+            this.btnBaseSingle.Click += new System.EventHandler(this.btnBaseSingle_Click);
+            // 
+            // cbxMode
+            // 
+            this.cbxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxMode.FormattingEnabled = true;
+            this.cbxMode.Items.AddRange(new object[] {
+            "Mode: Measure",
+            "Mode: Dump"});
+            this.cbxMode.Location = new System.Drawing.Point(3, 352);
+            this.cbxMode.Name = "cbxMode";
+            this.cbxMode.Size = new System.Drawing.Size(107, 21);
+            this.cbxMode.TabIndex = 9;
+            this.cbxMode.SelectedIndexChanged += new System.EventHandler(this.cbxMode_SelectedIndexChanged);
+            // 
+            // lblWatts
+            // 
+            this.lblWatts.AutoSize = true;
+            this.lblWatts.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWatts.Location = new System.Drawing.Point(12, 246);
+            this.lblWatts.Name = "lblWatts";
+            this.lblWatts.Size = new System.Drawing.Size(52, 17);
+            this.lblWatts.TabIndex = 10;
+            this.lblWatts.Text = "0 watts";
+            // 
+            // lblVolts
+            // 
+            this.lblVolts.AutoSize = true;
+            this.lblVolts.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolts.Location = new System.Drawing.Point(12, 212);
+            this.lblVolts.Name = "lblVolts";
+            this.lblVolts.Size = new System.Drawing.Size(49, 17);
+            this.lblVolts.TabIndex = 11;
+            this.lblVolts.Text = "0 volts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Volt Factor";
+            // 
+            // lblPowerFactor
+            // 
+            this.lblPowerFactor.AutoSize = true;
+            this.lblPowerFactor.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPowerFactor.Location = new System.Drawing.Point(12, 263);
+            this.lblPowerFactor.Name = "lblPowerFactor";
+            this.lblPowerFactor.Size = new System.Drawing.Size(52, 17);
+            this.lblPowerFactor.TabIndex = 14;
+            this.lblPowerFactor.Text = "0.00 PF";
+            // 
+            // txtVoltFactor
+            // 
+            this.txtVoltFactor.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::JeeAWattVis.Properties.Settings.Default, "VoltFactor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtVoltFactor.Location = new System.Drawing.Point(15, 150);
+            this.txtVoltFactor.Name = "txtVoltFactor";
+            this.txtVoltFactor.Size = new System.Drawing.Size(71, 20);
+            this.txtVoltFactor.TabIndex = 13;
+            this.txtVoltFactor.Tag = "voltFactor=";
+            this.txtVoltFactor.Text = global::JeeAWattVis.Properties.Settings.Default.VoltFactor;
+            // 
+            // chkShowLabels
+            // 
+            this.chkShowLabels.AutoSize = true;
+            this.chkShowLabels.Checked = global::JeeAWattVis.Properties.Settings.Default.PointLabels;
+            this.chkShowLabels.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShowLabels.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::JeeAWattVis.Properties.Settings.Default, "PointLabels", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.chkShowLabels.Location = new System.Drawing.Point(6, 329);
+            this.chkShowLabels.Name = "chkShowLabels";
+            this.chkShowLabels.Size = new System.Drawing.Size(80, 17);
+            this.chkShowLabels.TabIndex = 6;
+            this.chkShowLabels.Text = "Point labels";
+            this.chkShowLabels.UseVisualStyleBackColor = true;
+            this.chkShowLabels.CheckStateChanged += new System.EventHandler(this.chkShowLabels_CheckStateChanged);
+            // 
+            // txtAmpFactor
+            // 
+            this.txtAmpFactor.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::JeeAWattVis.Properties.Settings.Default, "AmpFactor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtAmpFactor.Location = new System.Drawing.Point(15, 111);
+            this.txtAmpFactor.Name = "txtAmpFactor";
+            this.txtAmpFactor.Size = new System.Drawing.Size(71, 20);
+            this.txtAmpFactor.TabIndex = 2;
+            this.txtAmpFactor.Tag = "ampFactor=";
+            this.txtAmpFactor.Text = global::JeeAWattVis.Properties.Settings.Default.AmpFactor;
+            this.txtAmpFactor.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAmpFactor_KeyDown);
+            this.txtAmpFactor.Leave += new System.EventHandler(this.txtAmpFactor_Leave);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 434);
+            this.Controls.Add(this.chtVis);
+            this.Controls.Add(this.pnlLeft);
+            this.DoubleBuffered = true;
+            this.Name = "frmMain";
+            this.Text = "JeeAWatt Visualization";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chtVis)).EndInit();
+            this.pnlLeft.ResumeLayout(false);
+            this.pnlLeft.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serial;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtVis;
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.Label lblVoltBase;
+        private System.Windows.Forms.Label lblAmpBase;
+        private System.Windows.Forms.Button btnBase;
+        private System.Windows.Forms.Label lblAmpTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmpFactor;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.CheckBox chkShowLabels;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnBaseSingle;
+        private System.Windows.Forms.ComboBox cbxMode;
+        private System.Windows.Forms.Label lblWatts;
+        private System.Windows.Forms.Label lblVolts;
+        private System.Windows.Forms.TextBox txtVoltFactor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPowerFactor;
+    }
+}
+
---
+++ arduino/JeeAWatt/visualize/JeeAWattVis/frmMain.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace JeeAWattVis
+{
+    public partial class frmMain : Form
+    {
+        private delegate void GraphDataHandler(int volt, int amp);
+        private delegate void ValueHandler(StringDictionary vals);
+
+        private Thread _serialThread;
+        private List<int> _dataVolt = new List<int>();
+        private List<int> _dataAmp = new List<int>();
+        private int _baseAmp = 0;
+        private int _baseVolt = 0;
+
+        private void Serial_Line(string s)
+        {
+            if (s.Equals("V,A"))
+                this.Invoke(new Action(GraphResetData));
+            else if (s.Equals("."))
+                this.Invoke(new Action(GraphDataDone));
+            else
+            {
+                string[] data = s.Split(',');
+                if (data.Length > 1)
+                {
+                    int v = 0, a = 0;
+                    if (Int32.TryParse(data[0], out v) && Int32.TryParse(data[1], out a))
+                        this.Invoke(new GraphDataHandler(GraphData), new object[] { v, a });
+                }
+                else
+                {
+                    data = s.Split(' ');
+                    if (data.Length > 0)
+                    {
+                        StringDictionary vals = new StringDictionary();
+                        foreach (var val in data)
+                        {
+                            string[] parts = val.Split('=');
+                            if (parts.Length > 1)
+                                vals.Add(parts[0], parts[1]);
+                        }
+                        if (vals.Count > 0)
+                            this.Invoke(new ValueHandler(OnValues), new object[] { vals });
+                    }
+                }
+            }
+        }
+
+        private void Serial_Execute()
+        {
+            try
+            {
+                while (serial.IsOpen)
+                {
+                    try
+                    {
+                        string s = serial.ReadLine();
+                        if (String.IsNullOrWhiteSpace(s))
+                            continue;
+                        Serial_Line(s);
+                    }
+                    catch (TimeoutException ex)
+                    { }
+                } /* while open */
+            }
+            catch (IOException e)
+            { 
+            }
+        }
+
+        private void OnValues(StringDictionary vals)
+        {
+            foreach (DictionaryEntry item in vals)
+            {
+                if ((string)item.Key == "ampbase")
+                {
+                    lblAmpBase.Text = "Amp Base: " + item.Value;
+                    _baseAmp = Int32.Parse((string)item.Value);
+                    Properties.Settings.Default.AmpBase = _baseAmp;
+                }
+                else if ((string)item.Key == "voltbase")
+                {
+                    lblVoltBase.Text = "Volt Base: " + item.Value;
+                    _baseVolt = Int32.Parse((string)item.Value);
+                    Properties.Settings.Default.VoltBase = _baseVolt;
+                }
+                else if ((string)item.Key == "cnt")
+                    lblCount.Text = "Count: " + item.Value;
+                else if ((string)item.Key == "a")
+                {
+                    lblCurrent.Text = item.Value + " amps";
+                    while (chtVis.Series[1].Points.Count > 60)
+                        chtVis.Series[1].Points.RemoveAt(0);
+                    chtVis.Series[1].Points.Add(Double.Parse((string)item.Value));
+                }
+                else if ((string)item.Key == "pf")
+                {
+                    lblPowerFactor.Text = (string)item.Value + " PF";
+                }
+                else if ((string)item.Key == "v")
+                {
+                    lblVolts.Text = (string)item.Value + " volts";
+                }
+                else if ((string)item.Key == "w")
+                    lblWatts.Text = item.Value + " watts";
+            }
+        }
+
+        private void GraphResetData()
+        {
+            _dataVolt.Clear();
+            _dataAmp.Clear();
+        }
+
+        private void GraphDataDone()
+        {
+            chtVis.Series[0].Points.DataBindY(_dataVolt);
+            chtVis.Series[1].Points.DataBindY(_dataAmp);
+
+            if (_baseAmp != 0)
+            {
+                int ampTotal = 0;
+                foreach (int amp in _dataAmp)
+                {
+                    ampTotal += Math.Abs(amp - _baseAmp);
+                }
+                double ea = (double)ampTotal / _dataAmp.Count;
+                lblAmpTotal.Text = String.Format("Amp total: {0}\n({1:f2}ea) ", 
+                    ampTotal, ea);
+
+                double f = 0.0;
+                if (Double.TryParse(txtAmpFactor.Text, out f) && f != 0.0)
+                {
+                    f = ea * f;
+                    lblCurrent.Text = String.Format("{0:f3} amps", f);
+                }
+            }  /* if baseamp */
+        }
+
+        private void GraphData(int volt, int amp)
+        {
+            _dataVolt.Add(volt);
+            _dataAmp.Add(amp);
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+            serial.ReadTimeout = 500;
+            serial.NewLine = "\n";
+             _serialThread = new Thread(Serial_Execute);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            serial.Open();
+            _serialThread.Start();
+            serial.WriteLine("");
+
+            _baseVolt = Properties.Settings.Default.VoltBase;
+            lblVoltBase.Text = "Volt Base: " + _baseVolt;
+            _baseAmp = Properties.Settings.Default.AmpBase;
+            lblAmpBase.Text = "Amp Base: " + _baseAmp;
+
+            cbxMode.SelectedIndex = Properties.Settings.Default.Mode;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            if (serial.IsOpen)
+            {
+                serial.Close();
+                _serialThread.Join();
+            }
+        }
+
+        private void btnBase_Click(object sender, EventArgs e)
+        {
+            SerialCommand("base");
+        }
+
+        private void SerialCommand(string p)
+        {
+            serial.WriteLine("/" + p);
+        }
+
+        private void chkShowLabels_CheckStateChanged(object sender, EventArgs e)
+        {
+            foreach (var srs in chtVis.Series)
+                srs.IsValueShownAsLabel = chkShowLabels.Checked;
+        }
+
+        private void btnBaseSingle_Click(object sender, EventArgs e)
+        {
+            SerialCommand("basesingle");
+        }
+
+        private void cbxMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Mode = cbxMode.SelectedIndex;
+            SerialCommand("dump=" + cbxMode.SelectedIndex);
+            switch (cbxMode.SelectedIndex)
+            {
+                case 0:
+                    lblAmpTotal.Text = "-";
+                    break;
+                case 1:
+                    lblWatts.Text = "- watts";
+                    break;
+            }
+            foreach (var srs in chtVis.Series)
+                srs.Points.Clear();
+        }
+
+        private void txtAmpFactor_Leave(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            double dummy;
+            if (Double.TryParse(txt.Text, out dummy))
+                SerialCommand(txt.Tag + txt.Text);
+        }
+
+        private void txtAmpFactor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtAmpFactor_Leave(sender, null);
+        }
+    }
+}
